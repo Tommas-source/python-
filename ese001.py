98fc8af21ce4a73d629fbf1de61c112fd37ffe41
unknown <aluno28008@mtorga.edu.pt>
2025-10-14 16:57:40 +0100

    adicionei uma estrutura condicional

---
+++ ese001.py
@@ -0,0 +1,7 @@
+nota1 = imput("digete a primeira nota :")
+nota2 = imput("digete a segunda nota :")
+
+soma = nota1 + nota2 
+
+
+print("a soma das notas é: []" .format(soma))
---
+++ meida.py
@@ -0,0 +1,12 @@
+nota1 = imput("digete a primeira nota :")
+nota2 = imput("digete a segunda nota :")
+
+soma = nota1 + nota2 
+
+
+print("a soma das notas é: []" .format(soma)) 
+  
+    print("aluno aprovado")
+else:
+    print("aluno aprovado")
+
