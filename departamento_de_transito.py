d466757b4b093344ede4483b728e98f5cc69bdf1
unknown <aluno28008@mtorga.edu.pt>
2025-10-21 16:55:34 +0100

    imc ex adicionais

---
+++ departamento_de_transito.py
@@ -0,0 +1,11 @@
+ano_atual = int(input("digite o ano atual : "))
+ano_nasc = int(input("digite o ano de nascimento : "))
+
+idade = ano_atual - ano_nasc
+print("o ano atual é [] e o ano de nascimento é [] a sua idade é [] anos". format(idade))
+
+if idade >= 18 : 
+    print("estas apto a conduzir.")
+else:
+    print("atencao!!! nao estas apto para conduzir.")
+    
